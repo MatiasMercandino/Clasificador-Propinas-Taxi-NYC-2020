71930c9dc82c07ffd5ea6014cecf5a234eba633e
Matías Mercandino <101747167+MatiasMercandino@users.noreply.github.com>
2024-07-29 14:53:00 -0400

    Update features.py

--- src/features.py
+++ src/features.py
@@ -5,7 +5,9 @@
 
 
 import pandas as pd
-import numpy as np
+from typing import List, Tuple
+
+target_col = "high_tip"
 
 numeric_feat = [
     "pickup_weekday",
@@ -22,37 +24,49 @@ categorical_feat = [
     "DOLocationID",
     "RatecodeID",
 ]
-features = numeric_feat + categorical_feat
+FEATURES = numeric_feat + categorical_feat
 EPS = 1e-7
 
-def preprocess(df, target_col):
+def build_features(df: pd.DataFrame, target_col: str) -> pd.DataFrame:
+    # Basic cleaning
     df = df[df['fare_amount'] > 0].reset_index(drop=True)  # avoid divide-by-zero
+    # add target
     df['tip_fraction'] = df['tip_amount'] / df['fare_amount']
     df[target_col] = df['tip_fraction'] > 0.2
+
+    # add features
     df['pickup_weekday'] = df['tpep_pickup_datetime'].dt.weekday
     df['pickup_hour'] = df['tpep_pickup_datetime'].dt.hour
     df['pickup_minute'] = df['tpep_pickup_datetime'].dt.minute
     df['work_hours'] = (df['pickup_weekday'] >= 0) & (df['pickup_weekday'] <= 4) & (df['pickup_hour'] >= 8) & (df['pickup_hour'] <= 18)
     df['trip_time'] = (df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']).dt.seconds
     df['trip_speed'] = df['trip_distance'] / (df['trip_time'] + EPS)
-    df = df[['tpep_dropoff_datetime'] + features + [target_col]]
-    df[features + [target_col]] = df[features + [target_col]].astype("float32").fillna(-1.0)
+
+    # drop unused columns
+    df = df[FEATURES + [target_col]]
+    df[FEATURES + [target_col]] = df[FEATURES + [target_col]].astype("float32").fillna(-1.0)
+
+    # convert target to int32 for efficiency (it's just 0s and 1s)
     df[target_col] = df[target_col].astype("int32")
+
     return df.reset_index(drop=True)
 
-def compare_distributions(dfa: pd.DataFrame, dfb: pd.DataFrame, features: list) -> pd.DataFrame:
+
+
+def compare_distributions(dfa: pd.DataFrame, dfb: pd.DataFrame) -> pd.DataFrame:
     """Compare the distributions of two datasets."""
     from scipy import stats
     statistics = []
     p_values = []
 
-    for feature in features:
+    for feature in FEATURES:
         statistic, p_value = stats.ks_2samp(dfa[feature], dfb[feature])
         statistics.append(statistic)
         p_values.append(p_value)
     
-    comparison_df = pd.DataFrame(data={'feature': features, 'statistic': statistics, 'p_value': p_values})
-    comparison_df = comparison_df.sort_values(by='p_value', ascending=True)
+    comparison_df = pd.DataFrame(data={'feature': FEATURES, 'statistic': statistics, 'p_value': p_values})
+    comparison_df.sort_values(by='p_value', ascending=True).head(11)
 
     return comparison_df
+
 
